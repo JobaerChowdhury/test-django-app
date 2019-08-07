ca17fd8a17ad9b9adea5f6e46c456c0e397ab768
Nikola Trandafilovic <nikola@elegantsolutions.no>
2019-08-07 10:49:42 +0200

    Updated db settings

--- workshop/settings.py
+++ workshop/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'workshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "test",
-#         "USER": "user",
-#         "PASSWORD": "123456",
-#         "HOST": "mysql",
-#         "PORT": "3306",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "test",
+        "USER": "user",
+        "PASSWORD": "123456",
+        "HOST": "mysql",
+        "PORT": "3306",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
